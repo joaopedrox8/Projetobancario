7f04566898d46ebe47d7829dbe2b496c01d3c9c6
unknown <joaocostapdev@gmail.com>
2026-09-26 00:55:57 -0300

    feat: add rota teste

--- src/main/java/com/example/sistemacontrolefinanceiro/controller/UsuarioController.java
+++ src/main/java/com/example/sistemacontrolefinanceiro/controller/UsuarioController.java
@@ -1,5 +1,15 @@
 package com.example.sistemacontrolefinanceiro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class UsuarioController {
+
+        @GetMapping("/teste")
+
+        public String teste() {
+            return "API funcionando no cu de neguinho";
+
+        }
 
 }
